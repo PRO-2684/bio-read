0506f02145a990c65e9a51a383f818137c4a6d0b
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-01-11 10:54:04 +0800

    doc: Update doc

--- README.md
+++ README.md
@@ -2,6 +2,26 @@
 
 Bionic reading in terminal.
 
+## 🚀 Installation
+
+If you have `cargo-binstall`, you can install this tool by running:
+
+```bash
+cargo binstall bio-read
+```
+
+Otherwise, you can install it from source:
+
+```bash
+cargo install bio-read
+```
+
+## 📖 Usage
+
+```bash
+cat file.txt | br
+```
+
 ## TODO
 
 - [ ] Allow streaming
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,9 @@ use bio_read::BioReader;
 
 fn main() {
     let reader = BioReader::new();
-    // let text = "An apple a day keeps the doctor away.";
     // Read from stdin until EOF
     let mut text = String::new();
-    std::io::stdin().read_to_string(&mut text).unwrap();
+    std::io::stdin().read_to_string(&mut text).expect("Failed to read from stdin");
     let bio_read_text = reader.bio_read_text(&text);
     println!("{}", bio_read_text);
 }
