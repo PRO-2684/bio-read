3ccb26b37f4d1f8ef3b0cae93e06b40a097b7c70
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2025-01-13 16:08:49 +0800

    chore: Code format; Integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::io::Read;
 use bio_read::BioReader;
+use std::io::Read;
 
 fn main() {
     let fixation_point = 3;
     let reader = BioReader::new().fixation_point(fixation_point);
     // Read from stdin until EOF
     let mut text = String::new();
-    std::io::stdin().read_to_string(&mut text).expect("Failed to read from stdin");
+    std::io::stdin()
+        .read_to_string(&mut text)
+        .expect("Failed to read from stdin");
     let bio_read_text = reader.bio_read_text(&text);
     println!("{}", bio_read_text);
 }
---
+++ tests/integration_test.rs
@@ -0,0 +1,32 @@
+use bio_read::BioReader;
+use std::{fs, path::Path};
+
+fn setup_reader(fixation_point: usize) -> BioReader {
+    let reader = BioReader::new()
+        .emphasize(|s| format!("<em>{s}</em>"))
+        .de_emphasize(|s| format!("<de>{s}</de>"))
+        .fixation_point(fixation_point);
+    reader
+}
+
+#[test]
+fn test_bio_read_simple() {
+    let reader = setup_reader(3);
+    assert_eq!(reader.bio_read_text("hello world"), "<em>hel</em><de>lo</de> <em>wor</em><de>ld</de>");
+}
+
+#[test]
+fn test_bio_read_on_files() {
+    let reader = setup_reader(3);
+    // tests/input/* -> tests/output/*
+    let files = fs::read_dir("tests/input").unwrap();
+    for file in files {
+        let file = file.unwrap();
+        let path = file.path();
+        let text = fs::read_to_string(&path).unwrap();
+        let output = reader.bio_read_text(&text);
+        let output_path = Path::new("tests/output").join(path.file_name().unwrap());
+        let expected_output = fs::read_to_string(&output_path).unwrap();
+        assert_eq!(output, expected_output);
+    }
+}
